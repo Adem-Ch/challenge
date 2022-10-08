2f56fe625b106cef5a4e98785044a71024ee8f5b
Adem <adem-ch@ya.ru>
2022-10-08 22:05:34 +0300

    Add array or data. Realised search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+import data from "./Data";
 
 function App() {
+  let inputRef = React.createRef();
+  function searchWord() {
+    let searchResult = "";
+
+    data.forEach((elem) => {
+      if (elem[1] === inputRef.current.value) searchResult = elem[0];
+    });
+
+    console.log(searchResult);
+  }
   return (
     <div className="App">
       <header className="App-header">
-       
-          Learn React
-        </a>
+        Learn React
+        <input type="text" ref={inputRef}></input>
+        <button onClick={searchWord}>Search</button>
       </header>
     </div>
   );
---
+++ src/Data.js
@@ -0,0 +1,7 @@
+const Data = [
+  ["one", "1"],
+  ["two", "2"],
+  ["three", "3"],
+];
+
+export default Data;
