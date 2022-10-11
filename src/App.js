c17179254bb3b7adda0ed3b851ed1c3bcb636aa7
Adem <adem-ch@ya.ru>
2022-10-11 23:01:54 +0300

    using state

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import data from "./Data";
 
 function App() {
   let inputRef = React.createRef();
+  const [rezValue, setRezValue] = React.useState("");
+
   function searchWord() {
     let searchResult = "";
 
     data.forEach((elem) => {
-      if (elem[1] === inputRef.current.value) searchResult = elem[0];
+      //      if (elem[1] === inputRef.current.value) searchResult = elem[0];
+      if (elem[1] === inputRef.current.value) setRezValue(elem[0]);
     });
 
-    console.log(searchResult);
+    console.log(rezValue);
   }
+
+  // state examples
+
   return (
     <div className="App">
       <header className="App-header">
         Learn React
         <input type="text" ref={inputRef}></input>
         <button onClick={searchWord}>Search</button>
+        <h1>Результат поиска - {rezValue}</h1>
       </header>
     </div>
   );
